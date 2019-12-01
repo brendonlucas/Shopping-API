f195e2d2f65c2e122a3f22f28ccdc7ea65dc254f
brendonlucas <brendonplay007@gmail.com>
2019-12-01 03:16:48 -0300

    refactore code

--- Controle_Shopping/urls.py
+++ Controle_Shopping/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='API')
+schema_view = get_swagger_view(title='API - Shopping')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- teste request api.py
+++ teste request api.py
@@ -2,20 +2,18 @@ import requests
 
 
 def main():
-    key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTc0Nzc4NTc5LCJqdGkiOiJhZDZhOWZiMDk4ZjI0ZDNmOWM0NTVjNzQ1NmRhYjE0ZiIsInVzZXJfaWQiOjF9.yxbXYqYJV-pYtwRZKQf4ZrCQ2OlTaTozOnGPHMGhZ3c'
-    key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTc0Nzc4NjgzLCJqdGkiOiJjZjhiMGRiYWM4MDA0OGQyYWIyNWVkZDFiOTQ5ZjAyMCIsInVzZXJfaWQiOjN9.eGRbasu8NC29oT89FoYjr0L08WSBIsPBaX4MPK-yE3Y'
-    key_refresh = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTU3NDg2NDA2NSwianRpIjoiYjA5NWNiNzRlZTNjNDkzMWFiY2QyM2FjYTc2NGQ5YTkiLCJ1c2VyX2lkIjoxfQ.IFS8JjMHmJWXbUOU_MxA53N9lSxtaTwo2pPIl-6kjks"
-    key_refresh = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTU3NDg2NDc4MywianRpIjoiOTA5OGExNjYzYzQwNDE3ZTk0Zjc4YjgyM2QxY2EyYmMiLCJ1c2VyX2lkIjozfQ.fOrjCToktN7bjkTDxOERRoFFh-4bSHcs0K9HLT3-6Q4'
-    consulta(key)
+    key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTc1MTc2OTE1LCJqdGkiOiJjODk4ZmE3YTBjMDk0ZmQyYjg5NTc5NTQ0ZjIyYTg0YSIsInVzZXJfaWQiOjEwfQ.hrY6vQjb8d2oB2IsEqZm_GgkXT1uRgCGCV-oHxVNItw'
+    key_refresh = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTU3NTI2MTQxNSwianRpIjoiM2NiZmI3OWRkZTdiNDc0MDkwYWI0MmIyZmQxZTA2ZDIiLCJ1c2VyX2lkIjoxMH0.uG5K_GFj0K5kb7Ru6VSO2DVxGp_cnLpIevOJS2rxWN4"
     # refresh_key(key_refresh)
-    # get_key("Lucas", "123456789")
 
+    # get_key("brendon", "123456789")
+    consulta(key)
+    # consulta2()
     print()
 
 
 def consulta(key=""):
     urlget = 'http://127.0.0.1:8000/clientes/'
-    urlget = 'http://127.0.0.1:8000/lojas/2/'
 
     if key != "":
         headers = {
@@ -25,6 +23,31 @@ def consulta(key=""):
         r = requests.get(urlget)
     print(r.json())
 
+
+def consulta2(key=""):
+    urlget = 'http://127.0.0.1:8000/lojas/'
+
+    if key != "":
+        headers = {
+            "Authorization": "Bearer " + key}
+        r = requests.get(urlget, headers=headers)
+    else:
+        r = requests.get(urlget)
+    print(r.json())
+
+def get_key(username, password):
+    url_key_get = 'http://127.0.0.1:8000/api/token/'
+    body = {
+        "username": username,
+        "password": password
+    }
+    r = requests.post(url_key_get, json=body)
+    if r.status_code == 200:
+        print("Key de acesso: ", r.json()['access'])
+        print("Key de atualizacao: ", r.json()['refresh'])
+    else:
+        print(r.json())
+        print('erro')
 
 def refresh_key(key):
     url_key_refresh = 'http://127.0.0.1:8000/api/token/refresh/'
@@ -36,15 +59,7 @@ def refresh_key(key):
     print(r.json())
 
 
-def get_key(username, password):
-    url_key_get = 'http://127.0.0.1:8000/api/token/'
-    body = {
-        "username": username,
-        "password": password
-    }
-    r = requests.post(url_key_get, json=body)
-    print("Key de acesso: ", r.json()['access'])
-    print("Key de atualizacao: ", r.json()['refresh'])
+
 
 
 def get_key_2():
